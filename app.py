31c615a2cc2806f046ce81a6aca9c536d03db97f
Th <thiago.rodrigues@1telecom.com.br>
2024-06-11 16:40:15 -0300

    ci cd test

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-# Rota para a página inicial
 @app.route('/')
 def index():
-    return 'test'
+    return 'Página inicial'
 
-# Rota para uma página de saudação com JSON
 @app.route('/api')
 def api():
     data = {'message': 'UP'}
-    return jsonify(data)
+    return data
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Verifica se a aplicação está sendo executada no ambiente do GitHub Actions
+    import os
+    if 'GITHUB_ACTIONS' in os.environ:
+        app.run(host='0.0.0.0', port=5000)  # Inicia o servidor Flask em um endereço acessível de qualquer lugar
+    else:
+        app.run(debug=True)  # Inicia o servidor Flask no modo de depuração se não estiver no ambiente do GitHub Actions
