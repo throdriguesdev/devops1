8901fcd0ecd35958f3f6078105153a9966b4ba1a
Th <thiago.rodrigues@1telecom.com.br>
2024-06-11 17:26:04 -0300

    docker + ci/cd test

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,17 @@
+# Use a imagem base do Python 3.9
 FROM python:3.9
 
+# Define o diretório de trabalho dentro do contêiner
 WORKDIR /app
 
+# Copia o conteúdo do diretório atual para o diretório de trabalho no contêiner
 COPY . /app
 
+# Atualiza o pip
 RUN pip install --upgrade pip
+
+# Instala as dependências listadas no requirements.txt
 RUN pip install -r requirements.txt
 
+# Comando padrão para executar a aplicação quando o contêiner for iniciado
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -12,9 +12,5 @@ def api():
     return data
 
 if __name__ == '__main__':
-    # Verifica se a aplicação está sendo executada no ambiente do GitHub Actions
-    import os
-    if 'GITHUB_ACTIONS' in os.environ:
-        app.run(host='0.0.0.0', port=5000)  # Inicia o servidor Flask em um endereço acessível de qualquer lugar
-    else:
-        app.run(debug=True)  # Inicia o servidor Flask no modo de depuração se não estiver no ambiente do GitHub Actions
+    app.run(host='0.0.0.0', port=5000)  # Alterado o host para '0.0.0.0' e adicionado o parâmetro 'port'
+
